d6391055edc4902db004787d32aace8603cdbc17
Ulfbei <109044345+Ulfbei@users.noreply.github.com>
2022-08-19 23:08:10 +0100

    finished project

--- controllers/PayController.js
+++ controllers/PayController.js
@@ -1,48 +1,43 @@
-class FormController{
-    form(req, res){
+class FormController {
+    form(req, res) {
         return res.render('form');
     }
 
-    check(req, res){
-        console.log(req.body.firstname);
-        console.log(req.body.lastname);
-        console.log(req.body.CVV);
-        console.log(req.body.PAN[0] + req.body.PAN[1]);
-        console.log(req.body.cardexpiry);
+    check(req, res) {
+        //get real time date
         const currentDate = new Date()
         let year = req.body.cardexpiry[0];
         for (let index = 1; index < 4; index++) {
-            year += req.body.cardexpiry[index];  
+            year += req.body.cardexpiry[index];
         }
-        console.log(year);
-        console.log(req.body.cardexpiry.toString().substr(5))
-
-
-        if (req.body.PAN.length < 16 || req.body.PAN.length >19)
-            return res.render('errordisplay', { response: 'Size matters'});
-        
+        //validate PAN number of digits
+        if (req.body.PAN.length < 16 || req.body.PAN.length > 19)
+            return res.render('errordisplay', { response: 'Size matters' });
+        //validate inserted year, if smaller than current time return false, if equal or highter return true
         if (year < currentDate.getFullYear())
-            return res.render('errordisplay', { response: 'Card Invalid'});
-        
-        if(year == currentDate.getFullYear())
-            if (req.body.cardexpiry.toString().substr(5) < (currentDate.getMonth()+1))
-                return res.render('errordisplay', { response: 'Card Invalid'});    
+            return res.render('errordisplay', { response: 'Card Out of Date' });
 
-        if(req.body.cardType == "AmericanExpress"){
-            
-            if(req.body.CVV.length !=4)
-                return res.render('errordisplay', { response: 'CVV must have 4 numbers'});
-                //se começa por 34 ou 37
-            if(req.body.PAN[0] !=3)
-                return res.render('errordisplay', { response: 'PAN must start with number 3'});
-            if(req.body.PAN[1] !=4 && req.body.PAN[1] !=7)
-                return res.render('errordisplay', { response: 'PAN second number must be 4 or 7'});
+        if (year == currentDate.getFullYear())
+            if (req.body.cardexpiry.toString().substr(5) < (currentDate.getMonth() + 1))
+                return res.render('errordisplay', { response: 'Card Out of Date' });
+        //verify if card is American Express
+        if (req.body.cardType == "AmericanExpress") {
+            //verify if CVV length is 4 digits
+            if (req.body.CVV.length != 4)
+                return res.render('errordisplay', { response: 'CVV must have 4 numbers' });
+            //verify if PAN first digit is 3
+            if (req.body.PAN[0] != 3)
+                return res.render('errordisplay', { response: 'PAN must start with number 3' });
+            //verify if PAN second digit is 4 or 7
+            if (req.body.PAN[1] != 4 && req.body.PAN[1] != 7)
+                return res.render('errordisplay', { response: 'PAN second number must be a 4 or 7' });
         }
-        else{
-            if(req.body.CVV.length != 3)
-            return res.status(403).json({error: "Error CVV length must be 3 numbers"});
+        //if the card is not Aexpress CVV must be 3 digits
+        else {
+            if (req.body.CVV.length != 3)
+                return res.render('errordisplay', { response: 'CVV must have 3 numbers' });
         }
-        return res.render('index', { title: 'Express' });
+        return res.render('index', { title: 'Form Successfully Completed' });
     }
 }
 
---
+++ readme.md
@@ -0,0 +1,28 @@
+# LevelUpSchool Evaluation Task
+
+# Credit Card Validator
+
+# Getting started
+
+## Requirements
+- [Node.js](https://nodejs.org/en/)
+- [npm](https://www.npmjs.com/)
+
+# Install the dependencies
+$ npm install
+
+# To the aplication
+$ npm start
+
+# For development mode
+$ npm run develop
+
+
+## Technologies
+
+Technologies tin this aplication
+
+- [Node.js](https://nodejs.org/en/)
+- [Express](https://expressjs.com/)
+- [nodemon](https://nodemon.io/)
+- [VS Code](https://code.visualstudio.com/)
